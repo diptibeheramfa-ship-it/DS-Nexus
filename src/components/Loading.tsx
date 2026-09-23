3015640a71c40b631966fa7f8817f7fd399868ca
diptibeheramfa-ship-it <diptibehera.mfa@gmail.com>
2026-09-23 21:34:45 +0530

    Delete src directory

--- src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-const Loading = () => {
-  return (
-    <div className="flex justify-center h-screen items-center">
-        <div className="animate-spin size-8 border-2 border-indigo-600 border-t-transparent rounded-full" />
-    </div>
-  )
-}
-
-export default Loading
--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module "*.jsx";
-declare module "html2pdf.js";
-
-interface NotificationOptions {
-  renotify?: boolean;
-  vibrate?: number | number[];
-}
